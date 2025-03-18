68d248c8459f77444deb15184a4f5aeed6649c41
popeye0618 <popeye0618@gmail.com>
2025-03-18 22:09:05 +0900

    feat: API 응답 모델 및 전역 예외 처리 정의

---
+++ src/main/java/emp/emp/util/api_response/ErrorCode.java
@@ -0,0 +1,27 @@
+package emp.emp.util.api_response;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorCode {
+
+	// General Error
+	BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청"),
+	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러"),
+
+	// Authentication Error
+	INVALID_OAUTH2_PROVIDER(HttpStatus.BAD_REQUEST, "잘못된 로그인 방식"),
+	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 유저"),
+	UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증되지 않음")
+	;
+
+	private final HttpStatus httpStatus;
+	private final String message;
+
+	ErrorCode(HttpStatus httpStatus, String message) {
+		this.httpStatus = httpStatus;
+		this.message = message;
+	}
+}
---
+++ src/main/java/emp/emp/util/api_response/Response.java
@@ -0,0 +1,63 @@
+package emp.emp.util.api_response;
+
+import java.io.IOException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Response<T> {
+	private HttpStatus status;
+	private String message;
+	private T data;
+
+	public static Response<Void> ok() {
+		Response<Void> response = new Response<>();
+		response.status = HttpStatus.OK;
+		return response;
+	}
+
+	public static <T> Response<T> ok(T data) {
+		Response<T> response = new Response<>();
+		response.status = HttpStatus.OK;
+		response.data = data;
+		return response;
+	}
+
+	public static <T> Response<T> errorResponse(ErrorCode errorCode) {
+		Response<T> response = new Response<>();
+		response.status = errorCode.getHttpStatus();
+		response.message = errorCode.getMessage();
+		response.data = null;
+		return response;
+	}
+
+	public String convertToJson() throws IOException {
+		ObjectMapper objectMapper = new ObjectMapper();
+		return objectMapper.writeValueAsString(this);
+	}
+
+	/**
+	 * 현재 Response 객체를 ResponseEntity로 변환하여 반환.
+	 *
+	 * @return ResponseEntity로 래핑된 Response 객체
+	 */
+	public ResponseEntity<Response<T>> toResponseEntity() {
+		HttpStatus httpStatus = this.status;
+		return new ResponseEntity<>(this, httpStatus);
+	}
+}
