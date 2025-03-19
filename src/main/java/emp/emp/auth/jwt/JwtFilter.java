d589f120ed6258afb33ce55032570afcbd7dea5e
popeye0618 <popeye0618@gmail.com>
2025-03-19 21:20:33 +0900

    feat: JWT 기능 추가

---
+++ src/main/java/emp/emp/auth/jwt/JwtFilter.java
@@ -0,0 +1,83 @@
+package emp.emp.auth.jwt;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import emp.emp.auth.custom.CustomUserDetails;
+import emp.emp.util.api_response.ErrorCode;
+import emp.emp.util.api_response.Response;
+import emp.emp.util.jwt.JwtTokenProvider;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+	private final JwtTokenProvider jwtTokenProvider;
+
+	private static final List<String> WHITELIST = List.of(
+		"/api/register",
+		"/api/login",
+		"/api/token/**"
+	);
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+		FilterChain filterChain) throws ServletException, IOException {
+
+		log.info("requestURI={}", request.getRequestURI());
+		if (WHITELIST.contains(request.getRequestURI())) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+
+		String accessToken = resolveToken(request);
+
+		if (accessToken != null) {
+			try {
+				if (jwtTokenProvider.validateToken(accessToken)) {
+					Claims claims = jwtTokenProvider.getClaims(accessToken);
+					CustomUserDetails userDetails = CustomUserDetails.createCustomUserDetailsFromClaims(claims);
+
+					UsernamePasswordAuthenticationToken authentication =
+						new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+					SecurityContextHolder.getContext().setAuthentication(authentication);
+
+				} else {
+					sendTokenRefreshResponse(response);
+					return;
+				}
+			} catch (Exception e) {
+				sendTokenRefreshResponse(response);
+				return;
+			}
+		}
+		filterChain.doFilter(request, response);
+	}
+
+	private String resolveToken(HttpServletRequest request) {
+		String bearerToken = request.getHeader("Authorization");
+		if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+			return bearerToken.substring(7);
+		}
+		return null;
+	}
+
+	private void sendTokenRefreshResponse(HttpServletResponse response) throws IOException {
+		response.setContentType("application/json;charset=UTF-8");
+		response.setStatus(HttpStatus.UNAUTHORIZED.value());
+		Response<Void> errorResponse = Response.errorResponse(ErrorCode.INVALID_ACCESS_TOKEN);
+		response.getWriter().write(errorResponse.convertToJson());
+	}
+}
