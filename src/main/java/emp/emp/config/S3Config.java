c8f2527dfa8b99b40ad49bb0daea30dc48acafd1
choi-jin-wook <jinwook219@gmail.com>
2025-05-16 22:55:32 +0900

    feat: 20250516 implement aws S3Config.java

--- src/main/java/emp/emp/config/S3Config.java
+++ src/main/java/emp/emp/config/S3Config.java
@@ -1,4 +1,24 @@
 package emp.emp.config;
 
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
 public class S3Config {
-}
+    @Value("${cloud.aws.credentials.accessKey}")
+    private String accessKey; // 엑세스 키
+
+    @Value("${cloud.aws.credentials.secretKey}")
+    private String secretKey; // 시크릿 키
+
+    @Value("${cloud.aws.region.static}")
+    private String region; // 리전 (지역)
+
+
+
