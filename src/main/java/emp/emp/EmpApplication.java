5945888b837df8d94917865adba8f2dee2826adb
popeye0618 <popeye0618@gmail.com>
2025-05-17 18:46:45 +0900

    chore: 설정 추가
    redis 설정 주석 해제
    Timezone 서울로 설정

--- src/main/java/emp/emp/EmpApplication.java
+++ src/main/java/emp/emp/EmpApplication.java
@@ -1,5 +1,7 @@
 package emp.emp;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class EmpApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(EmpApplication.class, args);
 	}
 
