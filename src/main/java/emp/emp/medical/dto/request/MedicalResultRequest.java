7c9dd84039080f8ee0063753f98700ebbc3a7743
SeungWon <semo970921@naver.com>
2025-05-18 22:48:32 +0900

    feat/요청DTO 생성

---
+++ src/main/java/emp/emp/medical/dto/request/MedicalResultRequest.java
@@ -0,0 +1,15 @@
+package emp.emp.medical.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MedicalResultRequest {
+
+  private String memo;
+  private Long prescriptionImageId;
+  private Long medicalImageId;
+  private boolean isPublic;
+
+}
