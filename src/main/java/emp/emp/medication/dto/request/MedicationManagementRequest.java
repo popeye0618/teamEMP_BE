919eac36dd79f502fa7fa0923443180c12621f25
SeungWon <semo970921@naver.com>
2025-06-01 14:25:43 +0900

    refactor/복약관리 등록/수정 요청하는 DTO 수정

--- src/main/java/emp/emp/medication/dto/request/MedicationManagementRequest.java
+++ src/main/java/emp/emp/medication/dto/request/MedicationManagementRequest.java
@@ -9,6 +9,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Getter
 @Setter
@@ -24,7 +25,20 @@ public class MedicationManagementRequest {
   @NotNull(message = "복약 종료일은 필수 입력사항 입니다.")
   private LocalDate endDate;  // 복약 종료일
 
+  @NotEmpty(message = "최소 1개 이상의 약물정보를 입력해주세요.")
+  @Valid
+  private List<MedicationDrugRequest> drugs; // 약물 정보 목록
 
 
+  /**
+   * 날짜 유효성 검증
+   * @return 유효 -> true, 유효x -> false
+   */
+  public boolean isValidDateRange(){
+    if(startDate == null || endDate == null){
+      return false; // 날짜가 null -> 유효X
+    }
+    return !endDate.isBefore(startDate); // 종료일이 시작일보다 빠름 -> 유효X
+  }
 
 }
